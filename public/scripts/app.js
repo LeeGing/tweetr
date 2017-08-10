86b3eb9ead2554f5425ea1a4166a6da72e1fd1f2
Edmond Lee <e.lee@live.ca>
2017-08-10 23:26:20 +0000

    Main database is now MongoDB.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -5,82 +5,36 @@
  */
 
 $( document ).ready(function() {
-// var tweets = [
-//   {
-//     "user": {
-//       "name": "Newton",
-//       "avatars": {
-//         "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-//         "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-//         "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-//       },
-//       "handle": "@SirIsaac"
-//     },
-//     "content": {
-//       "text": "If I have seen further it is by standing on the shoulders of giants"
-//     },
-//     "created_at": 1461116232227
-//   },
-//   {
-//     "user": {
-//       "name": "Descartes",
-//       "avatars": {
-//         "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
-//         "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
-//         "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
-//       },
-//       "handle": "@rd" },
-//     "content": {
-//       "text": "Je pense , donc je suis"
-//     },
-//     "created_at": 1461113959088
-//   },
-//   {
-//     "user": {
-//       "name": "Johann von Goethe",
-//       "avatars": {
-//         "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
-//         "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
-//         "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
-//       },
-//       "handle": "@johann49"
-//     },
-//     "content": {
-//       "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
-//     },
-//     "created_at": 1502246968
-//   }
-// ]
 
 // This following function is to prevent XSS.
-function escape(str) {
-  var div = document.createElement('div');
-  div.appendChild(document.createTextNode(str));
-  return div.innerHTML;
-}
+  function escape(str) {
+    var div = document.createElement('div');
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+  }
 
 //Creates a parent article containing all of the necessary elements for a tweet.
 //This element is rendered in composer-char-counter.js
-function createTweetElement(tweetData){
-    const html = 
-      `<article class="tweet-holder">
-        <header class ="tweet-cont-header">
-          <section class="at-picture">
-            <img src=' ${tweetData.user.avatars.small}'>
-          </section>
-          <section class="at-name"> ${tweetData.user.handle} </section>
-          <section class="user-name">${tweetData.user.name}</section>
-          <section class="separator"></section>
-        </header>
-        <div> ${escape(tweetData.content.text)}</div>
-        <footer> ${moment(tweetData.created_at).fromNow()}
-          <i class="fa fa-flag-o" aria-hidden="true"></i>
-          <i class="fa fa-refresh" aria-hidden="true"></i>
-          <i class="fa fa-heart-o" aria-hidden="true"></i>
-        </footer>
-      </article>`;
-      return html;
-}
+  function createTweetElement(tweetData){
+      const html = 
+        `<article class="tweet-holder">
+          <header class ="tweet-cont-header">
+            <section class="at-picture">
+              <img src=' ${tweetData.user.avatars.small}'>
+            </section>
+            <section class="at-name"> ${tweetData.user.handle} </section>
+            <section class="user-name">${tweetData.user.name}</section>
+            <section class="separator"></section>
+          </header>
+          <div> ${escape(tweetData.content.text)}</div>
+          <footer> ${moment(tweetData.created_at).fromNow()}
+            <i class="fa fa-flag-o" aria-hidden="true"></i>
+            <i class="fa fa-refresh" aria-hidden="true"></i>
+            <i class="fa fa-heart-o" aria-hidden="true"></i>
+          </footer>
+        </article>`;
+        return html;
+  }
 
 
 //This following function appends the outcome of the function from app.js.
@@ -119,7 +73,6 @@ function loadTweets(){
       })
     }
   });
-
 
 //With this event handler, the Compose button will now appear and disapear
 //with every click.
